3fefed8d2f01eb73bc088903227cba9e9a79d7ba
Héctor Selley <hector.selley@gmail.com>
2024-03-20 16:40:34 -0600

    Corrección de alineación

--- FoodPin/FoodPin/RestarantListView.swift
+++ FoodPin/FoodPin/RestarantListView.swift
@@ -28,9 +28,22 @@ struct RestarantListView: View {
         "barrafina", "donostia", "royaloak", "cask"
     ]
     
-    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    var restaurantLocations = [
+        "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong",
+        "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney",
+        "Sydney", "New York", "New York", "New York", "New York",
+        "New York", "New York", "New York", "London", "London",
+        "London", "London"
+    ]
             
-    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    var restaurantTypes = [
+        "Coffee & Tea Shop", "Cafe", "Tea House",
+        "Austrian / Causual Drink", "French", "Bakery", "Bakery",
+        "Chocolate", "Cafe", "American / Seafood", "American", 
+        "American", "Breakfast & Brunch", "Coffee & Tea",
+        "Coffee & Tea", "Latin American", "Spanish", "Spanish",
+        "Spanish", "British", "Thai"
+    ]
     
     var body: some View {
         List {
