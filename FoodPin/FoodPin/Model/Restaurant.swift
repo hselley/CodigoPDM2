0e3d4884c0606a417f1665f1ee1de746ee7fc22f
Héctor Selley <hector.selley@gmail.com>
2024-04-03 14:04:40 -0600

    Código completado hasta el capítulo 9
    Completé el código hasta el capítulo 9, en la página 256.

---
+++ FoodPin/FoodPin/Model/Restaurant.swift
@@ -0,0 +1,28 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Héctor Julián Selley Rojas on 03/04/24.
+//
+
+import Foundation
+
+struct Restaurant {
+    var name: String
+    var type: String
+    var location: String
+    var image: String
+    var isFavorite: Bool
+    
+    init(name: String, type: String, location: String, image: String, isFavorite: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.isFavorite = isFavorite
+    }
+    
+    init() {
+        self.init(name: "", type: "", location: "", image: "", isFavorite:false)
+    }
+}
